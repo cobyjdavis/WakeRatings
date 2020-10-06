d42188c4688273bc166d1e8dc02e9f9a9a6c4a3e
cobyjdavis <davic17@wfu.edu>
2020-10-06 18:25:21 -0400

    Initial Commit

---
+++ WakeRatings/WakeRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeRatingsApp.swift
+//  WakeRatings
+//
+//  Created by CJ Davis on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct WakeRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
